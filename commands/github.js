5853b28f61aa58debabcc1bcca9828e1c5628ea1
muhsink99 <muhsink99@users.noreply.github.com>
2018-08-31 00:04:58 +0100

    Added the github command

---
+++ commands/github.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'github',
+    description: 'Provides users with a link to the git repo.',
+    execute(message, args) {
+        message.channel.send("Here's a link to my repository: https://github.com/muhsink99/Zebot/blob/master/commands/music.js");
+    },
+};
