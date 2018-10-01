007f572da4123f17309fd49a3c6433e2a82e859c
muhsink99 <muhsink99@users.noreply.github.com>
2018-10-01 20:56:34 +0100

    fixed github command

--- commands/github.js
+++ commands/github.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'github',
     description: 'Provides users with a link to the git repo.',
     execute(message, args) {
-        message.channel.send("Here's a link to my repository: https://github.com/muhsink99/Zebot/blob/master/commands/music.js");
+        message.channel.send("Here's a link to my repository: https://github.com/muhsink99/Zebot");
     },
 };
